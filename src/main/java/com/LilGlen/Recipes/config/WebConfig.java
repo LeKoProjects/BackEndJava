a6f2c4f0f123e7254a6f89bfe8fe19e8bf99c900
leko <leopardorocha@gmail.com>
2024-11-30 14:22:46 -0300

    ADD

--- src/main/java/com/LilGlen/Recipes/config/WebConfig.java
+++ src/main/java/com/LilGlen/Recipes/config/WebConfig.java
@@ -8,9 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
-    
 }
