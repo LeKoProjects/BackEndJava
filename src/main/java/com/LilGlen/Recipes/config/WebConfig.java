cb15be9dbde8364b49f154097e5331edb03c35f5
leko <leopardorocha@gmail.com>
2024-12-04 15:32:12 -0300

    ADD

--- src/main/java/com/LilGlen/Recipes/config/WebConfig.java
+++ src/main/java/com/LilGlen/Recipes/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://adss-beige.vercel.app")
+            .allowedOrigins("https://adss-eight.vercel.app/")
             // .allowedOrigins("http://localhost:3000");
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
